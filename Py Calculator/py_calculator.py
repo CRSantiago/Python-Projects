36105ea541b3e2567334692a868ddf295a317818
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2020-01-11 23:01:59 -0500

    Added Exception handling when receiving user input

--- Py Calculator/py_calculator.py
+++ Py Calculator/py_calculator.py
@@ -1,8 +1,32 @@
 def prompt():
-  num1 = float(input("Enter the first number: "))
-  num2 = float(input("Enter second number: "))
-  answer = int(input("Which arithmetic function would you like to execute?\n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n"))
+  while True:
+    try:
+      num1 = float(input("Enter the first number: "))
+    except ValueError:
+      print("Please enter a valid number")
+      continue
+    else:
+      break
+
+  while True:
+    try:
+      num2 = float(input("Enter second number: "))
+    except ValueError:
+      print("Please enter a valid number")
+      continue
+    else:
+      break
+
+  while True:
+    try:
+      answer = int(input("Which arithmetic function would you like to execute?\n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n"))
+    except:
+      print("Please select option 1,2,3 or 4 from the menu.")
+      continue
+    else:
+      break
   return num1,num2,answer
+
 
 def addition(num1,num2):
   return num1 + num2
